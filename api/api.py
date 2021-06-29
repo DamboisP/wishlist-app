a72a171d27e2cd516a1ff10a18b6f7b86e3dabf0
Pierre Dambois <pierre@MacBook-Air-de-Pierre.local>
2021-06-29 18:04:43 +0200

    Using tinyDB for basic insert/updates

--- api/api.py
+++ api/api.py
@@ -1,44 +1,63 @@
 import flask
+from flask import request, abort
 from flask.helpers import make_response
 from flask.json import jsonify
 from datetime import datetime
 import time
+from tinydb import TinyDB, Query
+
+wishlist_db = TinyDB("wishlists.json")
 
 app = flask.Flask(__name__)
 app.config["debug"] = True
 
+# returns all wishlists
 @app.route('/', methods=['GET'])
-def home():
-    response = _generate_wishlists()
+def get_wishlists():
+    wishlists = wishlist_db.all()
+    result = [{'name' : wl['name']} for wl in wishlists] # not returning whole wishlists in 'all' call
+    return make_response(jsonify(result))
 
-    return make_response(jsonify(response))
+# creates a new wishlist
+@app.route('/', methods=['POST'])
+def new_wishlist():
+    if not request.json or not 'name' in request.json:
+        abort(400)
 
+    wishlist = {
+        'name' : request.json['name']
+    }
+
+    wishlist_db.insert(wishlist)
+
+    return make_response(jsonify(True))
+
+# get details from a wishlist
 @app.route('/<name>', methods=['GET'])
 def get_items(name):
     time.sleep(1) # To test loader
-    response = {
-        "name" : name,
-        "items" : _generate_items()
-    }
+    Wishlists = Query()
+    result = wishlist_db.search(Wishlists.name == name)
 
-    return make_response(jsonify(response))
+    if (result is None or len(result) == 0):
+        abort(404, description= "Could not find wishlist " + name)
 
-def _generate_wishlists():
-    wishlists = []
+    return make_response(jsonify(result))
 
-    for i in range(10):
-        wishlists.append(
-            { "name" : "Wishlist " + str(i + 1) }
-        )
-    return wishlists
+# updates a wishlist
+@app.route('/<name>', methods=['POST'])
+def update_wishlist(name):
+    if not request.json or not 'name' in request.json or not 'items' in request.json:
+        abort(400)
 
-def _generate_items():
-    items = []
+    Wishlists = Query()
+    result = wishlist_db.search(Wishlists.name == name)
 
-    for i in range(5):
-        items.append(
-            { "name" : "Item " + str(i + 1) }
-        )
-    return items
+    if (result is None or len(result) == 0):
+        abort(404, description= "Could not find wishlist " + name)
 
-app.run()
+    wishlist_db.update({'items': request.json['items']}, Wishlists.name == name)
+
+    return make_response(jsonify(True))
+
+app.run(debug=True)
